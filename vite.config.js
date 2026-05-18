87afa23f99a31c5970ddcb031c821a4e665d3ce8
zhongzihang5-cell <zhongzihang5@gmail.com>
2026-05-18 18:25:03 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jiu-gong-ge/',  // ← 加这一行
-  plugins: [react()],
   base: '/jiu-gong-ge/',
+  plugins: [react()],
 })
